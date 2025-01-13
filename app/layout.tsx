11b50c14e2c9ffb1efa398da18456be8a0c2f517
Aiden Zepp <aiden.zepp@protonmail.com>
2025-01-12 23:17:56 -0500

    fix: remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner"
-import Link from "next/link";
 import "./globals.css";
-import { ServiceProvider } from "@/contexts/page";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
